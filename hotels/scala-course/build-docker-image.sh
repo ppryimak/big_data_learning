64bde68af627a83ff0b5a5651f7de7a5767e3d26
ppryimak <pryimak@i.ua>
2018-04-21 19:16:38 +0300

    Added docker file and script to build and run this docker container.

---
+++ hotels/scala-course/build-docker-image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+APP_NAME=play-scala-server                                                     # Name of the app
+VERSION=0.0.1                                                      # App version
+USERNAME=5storm5                                                     # Docker Hub username
+
+echo "stopping container $APP_NAME"
+docker stop ${APP_NAME} && docker rm ${APP_NAME}
+
+
+echo "building $APP_NAME"
+sbt playUpdateSecret dist                                       # Update app secret and build Play app
+
+cd ./server/target/universal                                           # Go to directory where application zip is located
+rm -rf ./tmp ./dist                                             # Deleted existing directories if those exist
+
+unzip ./${APP_NAME}*.zip -d ./tmp                               # Extract application zip
+mv ./tmp/${APP_NAME}* ./dist                                    # Move application files into dist folder
+rm -rf ./tmp                                                    # Remove temp directory
+cd ../../..                                                        # Go back to application root
+
+echo "building docker image $USERNAME/$APP_NAME:$VERSION "
+docker build -t ${USERNAME}/${APP_NAME}:${VERSION} .            # Build docker image
+
+echo "running docker image $USERNAME/$APP_NAME:$VERSION "
+docker run --name ${APP_NAME} -p 8080:8080 -d ${USERNAME}/${APP_NAME}:${VERSION}
+
