d7e574271132d0fc1301a26a96c5cb42f378aa3b
ppryimak <pryimak@i.ua>
2018-04-22 09:33:01 +0300

    Added hdp scripts to vcs.

---
+++ hotels/hdp-scripts/hotels_input.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+FOLDER=/hotinput
+ACTION=$1
+
+if [ "$ACTION" = "create" ]; then
+	echo "Creating folder $FOLDER"
+	hadoop fs -mkdir ${FOLDER}
+	echo Folder is created
+elif [ "$ACTION" = "remove" ]; then
+      	echo "Removing folder $FOLDER"
+	hadoop fs -rm -r $FOLDER
+	echo Folder is removed
+
+else
+  echo "Unsupported oparation"
+fi
+
+hadoop fs -ls /
