54d1c0531c553de6013582ca280d2f8159c94a31
ppryimak <pryimak@i.ua>
2018-04-27 20:52:43 +0300

    Added simple spark example.

---
+++ bycycles/scala-spark-jobs/src/main/scala/com/bigdata/example/HelloScala.scala
@@ -0,0 +1,26 @@
+package com.bigdata.example
+
+import org.apache.spark.{SparkConf, SparkContext}
+
+object HelloScala extends App {
+  println("Hello Scala")
+
+  //Create a SparkContext to initialize Spark
+  val conf = new SparkConf()
+  conf.setMaster("local")
+  conf.setAppName("Word Count")
+  val sc = new SparkContext(conf)
+
+  val nameNode = "hdfs://sandbox-hdp.hortonworks.com:8020"
+  // Load the text into a Spark RDD, which is a distributed representation of each line of text
+  val textFile = sc.textFile(nameNode + "/tmp/shakespeare.txt")
+
+  //word count
+  val counts = textFile.flatMap(line => line.split(" "))
+    .map(word => (word, 1))
+    .reduceByKey(_ + _)
+
+  counts.foreach(println)
+  System.out.println("Total words: " + counts.count());
+  counts.saveAsTextFile(nameNode + "/tmp/shakespeareWordCount");
+}
